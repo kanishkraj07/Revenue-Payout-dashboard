9d7f36c9c8e2e88f9692ecf961470705130a3673
kanishkraj07 <kanishkmogalraj@gmail.com>
2024-07-28 02:11:42 +0530

    imrpoved css in header

--- src/components/PayoutHeader.jsx
+++ src/components/PayoutHeader.jsx
@@ -7,11 +7,11 @@ export function PayoutHeader() {
 
     return <div>
         {isMenu && <div className="md:hidden" onClick={() => setIsMenu(false)}><NavBar /></div> } 
-    <div className="grid grid-cols-3 md:grid-cols-12 bg-white p-5 px-0">
-        <div className="md:col-span-3 flex justify-start items-center gap-3">
+    <div className="grid grid-cols-3 md:grid-cols-12 bg-white p-5 px-0 md:place-items-start">
+        <div className="md:col-span-3 flex justify-start items-center gap-3 md:gap-0">
         <button onClick={() => setIsMenu(true)}><Menu className='md:hidden pl-3 text-black' size="45" /></button>
-            <div className="text-black text-md sm:text-2xl">Instant Pay</div>
-            <div className="invisible md:visible md:flex justify-center items-center gap-2 text-gray-500">
+            <div className="text-lg text-black text-md sm:text-2xl">Instant Pay</div>
+            <div className="invisible md:visible md:flex justify-center items-center gap-1 text-gray-500 md:ml-1">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
                 </svg>
